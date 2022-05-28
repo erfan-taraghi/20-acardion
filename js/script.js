d7f305de54bad8e89b34b9423a2646167fbc4678
erfan-taraghi <taraghikonerfan@gmail.com>
2022-05-28 11:58:41 +0430

    acardion

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const accor = document.querySelectorAll('.acardion-title');
+accor.forEach(item =>{
+    item.addEventListener('click',function(){
+        item.classList.toggle('active');
+        const content = item.nextElementSibling;
+        if(content.style.height){
+            content.style.height = null
+        }else{
+            content.style.height = content.scrollHeight + 'px';
+        }
+    })
+})
